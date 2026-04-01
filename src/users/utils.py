6e22c53eca7d574f45cb1a695b981a6ccf69362f
ruldak <ruldak3306@gmail.com>
2026-04-01 23:00:23 +0700

    add QueryTools in utils

--- src/users/utils.py
+++ src/users/utils.py
@@ -20,3 +20,56 @@ from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer, JwtRefresh
 secret_key = os.getenv("SECRET_KEY")
 access_security = JwtAccessBearer(secret_key=secret_key, auto_error=True)
 refresh_security = JwtRefreshBearer(secret_key=secret_key, auto_error=True)
+
+# ---------- ReActAgent tools ----------
+from llama_index.core import Settings, VectorStoreIndex
+from llama_index.embeddings.huggingface_api import HuggingFaceInferenceAPIEmbedding
+from llama_index.core.tools import QueryEngineTool
+import chromadb
+from llama_index.llms.openrouter import OpenRouter
+from llama_index.vector_stores.chroma import ChromaVectorStore
+from llama_index.core.vector_stores import MetadataFilters, ExactMatchFilter
+from llama_index.llms.groq import Groq
+from llama_index.core.vector_stores import MetadataFilter, MetadataFilters, FilterOperator
+
+class QueryTools:
+    """docstring for ClassName"""
+    def __init__(self):
+        self.api_key = os.getenv("API_KEY")
+        self.tenant = os.getenv("TENANT")
+        self.database = "fastapi_qna_legal_documents"
+        self.open_router_api_key = os.getenv("OPEN_ROUTER_API_KEY")
+        self.huggingface_api_key = os.getenv("HUGGING_FACE_API_KEY")
+        self.groq_api_key = os.getenv("GROQ_API_KEY")
+
+    def llm(self):
+        return Groq(model="llama-3.3-70b-versatile", temperature=0.2, api_key=self.groq_api_key)
+
+    def client(self):
+        return chromadb.CloudClient(
+            api_key=self.api_key,
+            tenant=self.tenant,
+            database=self.database
+        )
+
+    def query_documents(self, document_id: int):
+        """Answer questions based on specific documents."""
+        client = self.client()
+        Settings.llm = self.llm()
+
+        embed_model = HuggingFaceInferenceAPIEmbedding(
+            model_name="intfloat/multilingual-e5-large",
+            token=self.huggingface_api_key
+        )
+
+        collection = client.get_or_create_collection("legal_documents")
+        vstore = ChromaVectorStore(chroma_collection=collection)
+        index = VectorStoreIndex.from_vector_store(vstore, embed_model=embed_model)
+
+        filters = MetadataFilters(
+            filters=[
+                MetadataFilter(key="id", value=document_id, operator=FilterOperator.EQ)
+            ]
+        )
+
+        return index.as_query_engine(filters=filters, similarity_top_k=2, response_mode="tree_summarize")
